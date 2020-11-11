5560147436a8fb358aef57855f609166ce9393a9
Naman <patwanaman@gmail.com>
2020-11-11 09:34:50 +0000

    added module

---
+++ rgterraform/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group" {
+  description = "The name of your Azure Resource Group."
+  default     = "DN-VM-Test"
+}
+
+variable "location" {
+  description = "The region where the virtual network is created."
+  default     = "centralus"
+}
